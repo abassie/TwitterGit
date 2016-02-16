b1c2339794e6afba837bb902de88e3a5ba5271f2
Abby Bassie <alb699@msstate.edu>
2016-02-15 20:34:06 -0600

    progress

---
+++ Twitter/User.swift
@@ -0,0 +1,25 @@
+//
+//  User.swift
+//  Twitter
+//
+//  Created by Abby  Bassie on 2/15/16.
+//  Copyright © 2016 codepath. All rights reserved.
+//
+
+import UIKit
+
+class User: NSObject {
+    var name: String?
+    var screenname: String?
+    var profileImageURL: String?
+    var tagline: String?
+    var dictionary: NSDictionary
+    
+    init(dictionary: NSDictionary) {
+        self.dictionary = dictionary
+        name = dictionary["name"] as? String
+        screenname = dictionary["screen_name"] as? String
+        profileImageURL = dictionary["profile_image_url"] as? String
+        tagline = dictionary["descrition"] as? String
+    }
+}
