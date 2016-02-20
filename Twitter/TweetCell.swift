334e60464c90f42aa84994663db29fe5ea172843
Abby Bassie <alb699@msstate.edu>
2016-02-20 17:43:09 -0600

    working

---
+++ Twitter/TweetCell.swift
@@ -0,0 +1,42 @@
+//
+//  TweetCell.swift
+//  Twitter
+//
+//  Created by Abby  Bassie on 2/20/16.
+//  Copyright © 2016 codepath. All rights reserved.
+//
+
+import UIKit
+
+class TweetCell: UITableViewCell {
+
+    @IBOutlet weak var thumbImageView: UIImageView!
+    
+    @IBOutlet weak var userLabel: UILabel!
+    
+    @IBOutlet weak var tweetLabel: UILabel!
+    
+    var tweet: Tweet! {
+        didSet {
+            //ask Chase about this syntax
+            userLabel.text = tweet.user?.name
+            tweetLabel.text = tweet?.text
+            //thumbImageView.setImageWithURL((tweet?.user!.profileImageURL)!)
+            
+        }
+    }
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
