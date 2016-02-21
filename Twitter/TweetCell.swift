b793fee421ce012af7c5798e3db669bcbfce2223
Abby Bassie <alb699@msstate.edu>
2016-02-20 19:54:10 -0600

    dead_btns

--- Twitter/TweetCell.swift
+++ Twitter/TweetCell.swift
@@ -16,13 +16,45 @@ class TweetCell: UITableViewCell {
     
     @IBOutlet weak var tweetLabel: UILabel!
     
+    
+    @IBOutlet weak var likeBtn: UIButton!
+    
+    @IBOutlet weak var rtBtn: UIButton!
+    
     var tweet: Tweet! {
         didSet {
             //ask Chase about this syntax
             userLabel.text = tweet.user.name
             tweetLabel.text = tweet?.text
             thumbImageView.setImageWithURL((tweet?.user.profileImageURL)!)
+            setBtnStates()
             
+        }
+    }
+    
+    func setBtnStates() {
+        if let retweeted = tweet?.retweeted {
+            if retweeted {
+                rtBtn.setBackgroundImage(UIImage(named: "rt_on"), forState: .Normal)
+                rtBtn.enabled = false
+            }
+            else {
+                rtBtn.setBackgroundImage(UIImage(named: "rt_off"), forState: .Normal)
+                rtBtn.enabled = true
+            }
+            
+        }
+    
+    
+    if let liked = tweet?.liked {
+        if liked {
+            likeBtn.setBackgroundImage(UIImage(named: "like_on"), forState: .Normal)
+            likeBtn.enabled = false
+        }
+        else {
+            likeBtn.setBackgroundImage(UIImage(named: "like_off"), forState: .Normal)
+            likeBtn.enabled = true
+        }
         }
     }
     
@@ -35,8 +67,22 @@ class TweetCell: UITableViewCell {
 
     override func setSelected(selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
+        
+        rtBtn.setBackgroundImage(UIImage(named: "rt_on"), forState: .Disabled)
+        likeBtn.setBackgroundImage(UIImage(named: "rt_off"), forState: .Disabled)
+        
+        let btnTapped = UITapGestureRecognizer(target: self, action: "tappedUserImageView")
+        btnTapped.numberOfTapsRequired = 1
+        thumbImageView.addGestureRecognizer(btnTapped)
+        
 
         // Configure the view for the selected state
+    }
+    
+    override func prepareForReuse() {
+        thumbImageView.image = nil
+        setBtnStates()
+        
     }
 
 }
